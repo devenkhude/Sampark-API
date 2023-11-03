d104bff3de8237b0f083f096b7d336f033f9d44f
Devendra Khude <mr.devendrakhude@gmail.com>
2023-11-03 22:52:27 +0530

    Redis server added

--- subjectmasters/subjectmaster.service.js
+++ subjectmasters/subjectmaster.service.js
@@ -18,8 +18,30 @@ module.exports = {
   delete: _delete,
 };
 
-async function processWithDepartments(subjectMasters, departmentMasters) {
+/**
+ * To get all department subjects
+ * @returns department wise subjects list
+ * */
+async function getAllWithDepartments() {
+  // Fetch subject and department data with selective projection
   try {
+    const [subjectMasters, departmentMasters] = await Promise.all([
+      Subjectmaster.find()
+        .sort({ sort_order: 1 })
+        .select(
+          "id name icon activeicon module is_default registration_name for_registration"
+        ),
+      Departmentmaster.find()
+        .sort({ sort_order: 1 })
+        .select("id name subjects"),
+    ]);
+
+    if (!subjectMasters.length || !departmentMasters.length) {
+      throw new Error("No data found.");
+    }
+
+    // Assuming subject_masters and department_masters are arrays
+
     const subjects = {};
     const finalData = {
       sss: [],
@@ -79,122 +101,6 @@ async function processWithDepartments(subjectMasters, departmentMasters) {
     console.error("Catch Exception: ", err);
     throw new Error(err.message);
   }
-}
-
-/**
- * To get all department subjects
- * @returns department wise subjects list
- * */
-async function getAllWithDepartments() {
-  try {
-    let subjectMasters = [];
-    let departmentMasters = [];
-    const keys = ["AllSubjectMaster", "AllDepartmentMaster"];
-    redis.mget(keys, async (err, values) => {
-      if (values[0].length || values[1].length) {
-        console.log("Inside if statement");
-        subjectMasters = JSON.parse(values[0]);
-        departmentMasters = JSON.parse(values[1]);
-        const finalData = processWithDepartments(subjectMasters, departmentMasters);
-        return finalData;
-      } else {
-        console.log("Inside else statement");
-        subjectMasters = await Subjectmaster.find()
-          .sort({ sort_order: 1 })
-          .select(
-            "id name icon activeicon module is_default registration_name for_registration"
-          );
-        departmentMasters = await Departmentmaster.find()
-          .sort({ sort_order: 1 })
-          .select("id name subjects");
-        redis.set("AllSubjectMaster", JSON.stringify(values[0]));
-        redis.set("AllDepartmentMaster", JSON.stringify(values[1]));
-        const finalData = processWithDepartments(subjectMasters, departmentMasters);
-        return finalData;
-      }
-    });
-  } catch (err) {
-    console.error("Catch Exception: ", err);
-    throw new Error(err.message);
-  }
-
-  // Fetch subject and department data with selective projection
-  // const [subjectMasters, departmentMasters] = await Promise.all([
-  //   Subjectmaster.find()
-  //     .sort({ sort_order: 1 })
-  //     .select(
-  //       "id name icon activeicon module is_default registration_name for_registration"
-  //     ),
-  //   Departmentmaster.find()
-  //     .sort({ sort_order: 1 })
-  //     .select("id name subjects"),
-  // ]);
-
-  // if (!subjectMasters.length || !departmentMasters.length) {
-  //   throw new Error("No data found.");
-  // }
-
-  // Assuming subject_masters and department_masters are arrays
-
-  //   const subjects = {};
-  //   const finalData = {
-  //     sss: [],
-  //     sst: [],
-  //     ssh: [],
-  //     sa: [],
-  //     elearning: [],
-  //     dd: [],
-  //   };
-
-  //   // Process subject_masters
-  //   subjectMasters.forEach((subject) => {
-  //     const subjectId = subject.id;
-  //     subjects[subjectId] = {
-  //       id: subjectId,
-  //       name: subject.name,
-  //       icon: subject.icon
-  //         ? config.repositoryHost + subject.icon
-  //         : config.assetHost + subject.name.toLowerCase() + ".png",
-  //       icon_active: subject.activeicon
-  //         ? config.repositoryHost + subject.activeicon
-  //         : config.assetHost + "big-" + subject.name.toLowerCase() + ".png",
-  //       module: subject.module,
-  //       is_default: subject.is_default,
-  //       registration_name: subject.registration_name || subject.name,
-  //       for_registration: subject.for_registration || false,
-  //       departments: [],
-  //     };
-  //   });
-
-  //   // Process department_masters
-  //   departmentMasters.forEach((department) => {
-  //     const departmentSubjects = department.subjects;
-  //     departmentSubjects.forEach((depSubject) => {
-  //       const departmentMaster = {
-  //         id: department.id,
-  //         name: department.name,
-  //         is_default: subjects[depSubject].departments.length === 0,
-  //       };
-  //       subjects[depSubject].departments.push(departmentMaster);
-  //     });
-  //   });
-
-  //   // Create subjects_with_departments and populate finalData
-  //   const subjectsWithDepartments = Object.values(subjects);
-  //   subjectsWithDepartments.forEach((value) => {
-  //     if (finalData[value.module].length === 0) {
-  //       value.is_default = true;
-  //     } else {
-  //       value.is_default = false;
-  //     }
-  //     finalData[value.module].push(value);
-  //   });
-
-  //   return finalData;
-  // } catch (err) {
-  //   console.error("Catch Exception: ", err);
-  //   throw new Error(err.message);
-  // }
 }
 
 /**
