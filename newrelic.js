b12d7a1a75460266637222a1ab11b3e922434e88
Devendra Khude <mr.devendrakhude@gmail.com>
2024-08-25 14:28:34 +0530

    New relic file added

--- newrelic.js
+++ newrelic.js
@@ -1,7 +1,79 @@
+'use strict';
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config/default.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
 exports.config = {
-    app_name: ['Sampark-API-V83'],
-    license_key: '692554117ef03f9a0a87163ae6b98a4cFFFFNRAL',
-    logging: {
-      level: 'info',
-    },
-  };
+  /**
+   * Array of application names.
+   */
+  app_name: ['Sampark-API-V83'],
+
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '692554117ef03f9a0a87163ae6b98a4cFFFFNRAL',
+
+  /**
+   * This setting controls distributed tracing.
+   * Distributed tracing lets you see the path that a request takes through your distributed system.
+   */
+  distributed_tracing: {
+    /**
+     * Enables/disables distributed tracing.
+     *
+     * @env NEW_RELIC_DISTRIBUTED_TRACING_ENABLED
+     */
+    enabled: true
+  },
+
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info',
+    /**
+     * Path to the log file.
+     */
+    filepath: 'newrelic_agent.log'
+  },
+
+  /**
+   * This setting is used to ensure that the application remains instrumented by
+   * the New Relic agent even when the agent is unable to connect to the New Relic
+   * servers.
+   */
+  allow_all_headers: true,
+
+  /**
+   * Attributes allow you to collect extra information from requests
+   * and store them with the transaction event.
+   */
+  attributes: {
+    /**
+     * When true, the agent captures attributes from all headers
+     * except for those listed in attributes.exclude
+     */
+    enabled: true,
+    /**
+     * Prefix of headers to exclude from capture.
+     */
+    exclude: [
+      'request.headers.cookie',
+      'request.headers.authorization',
+      'request.headers.proxyAuthorization',
+      'request.headers.setCookie*',
+      'request.headers.x*',
+      'response.headers.cookie',
+      'response.headers.authorization',
+      'response.headers.proxyAuthorization',
+      'response.headers.setCookie*',
+      'response.headers.x*'
+    ]
+  }
+};
